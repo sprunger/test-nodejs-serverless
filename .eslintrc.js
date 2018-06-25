17556fd981a0315253ff880fecc06334f75b8797
Scott Sprunger <github@globaleworks.com>
2018-06-25 13:00:12 -0400

    Add Google Style Guide to eslint.  Remove dotenv package.  Add lint yarn script.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   parserOptions: { 
     ecmaVersion: 6 
   },
-  extends: ['eslint:recommended', 'prettier'], // extending recommended config and config derived from eslint-config-prettier
+  extends: ['eslint:recommended', 'google', 'prettier'], // extending recommended config and config derived from eslint-config-prettier
   plugins: ['prettier'], // activating esling-plugin-prettier (--fix stuff)
   rules: {
     'prettier/prettier': [ // customizing prettier rules (unfortunately not many of them are customizable)
